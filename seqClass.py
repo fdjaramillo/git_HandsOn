78e26170e1bb50364568dab115ac22114b9b5a4b
David <davidjaramilloe@uic.es>
2025-01-27 22:18:32 +0100

    script with some comments added

--- seqClass.py
+++ seqClass.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
+# Import necessary libraries
 import sys, re
 from argparse import ArgumentParser
 
+# Set up argument parser
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
+# Print help and exit if no arguments are provided
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
+# Parse the input arguments
 args = parser.parse_args()
+args.seq = args.seq.upper()
 
-args.seq = args.seq.upper()                 # Note we just added this line
+# Classify the sequence
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
@@ -24,6 +29,7 @@ if re.search('^[ACGTU]+$', args.seq):
 else:
     print ('The sequence is not DNA nor RNA')
 
+# Perform motif search if a motif is provided
 if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
